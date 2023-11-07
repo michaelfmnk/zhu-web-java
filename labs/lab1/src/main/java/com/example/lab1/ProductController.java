8673595e9b06f9e527e9b65e41df9b4055da1f10
michaelfmnk <michael.fmnk@gmail.com>
2023-11-07 18:15:05 +0200

    added submitted lab 1

---
+++ labs/lab1/src/main/java/com/example/lab1/ProductController.java
@@ -0,0 +1,70 @@
+package com.example.lab1;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private List<Product> products = new ArrayList<>();
+    private Long idCounter = 1L;
+    public ProductController() {
+        // Додавання початкового продукту
+        products.add(new Product(1L, "Назва продукту", "Опис продукту", 50.0));
+        idCounter++;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Long id) {
+        Product product = products.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(@RequestBody Product product) {
+        if (product.getId() != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id must be generated automatically");
+        }
+        product.setId(idCounter);
+        products.add(product);
+        idCounter++;
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId().equals(id)) {
+                products.set(i, updatedProduct);
+                return ResponseEntity.ok(updatedProduct);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) {
+                products.remove(product);
+                return ResponseEntity.ok("Product deleted");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+    }
+}
